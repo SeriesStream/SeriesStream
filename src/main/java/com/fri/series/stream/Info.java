6387935ad10929b9b8418904f4bc6a625ad9c504
Marko <marko.laviner>
2017-12-04 08:35:13 +0100

    info added

---
+++ src/main/java/com/fri/series/stream/Info.java
@@ -0,0 +1,90 @@
+/*
+ *  Copyright (c) 2014-2017 Kumuluz and/or its affiliates
+ *  and other contributors as indicated by the @author tags and
+ *  the contributor list.
+ *
+ *  Licensed under the MIT License (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  https://opensource.org/licenses/MIT
+ *
+ *  The software is provided "AS IS", WITHOUT WARRANTY OF ANY KIND, express or
+ *  implied, including but not limited to the warranties of merchantability,
+ *  fitness for a particular purpose and noninfringement. in no event shall the
+ *  authors or copyright holders be liable for any claim, damages or other
+ *  liability, whether in an action of contract, tort or otherwise, arising from,
+ *  out of or in connection with the software or the use or other dealings in the
+ *  software. See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+package com.fri.series.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Benjamin Kastelic
+ * @since 2.3.0
+ */
+public class Info {
+
+    private List<String> github;
+    private List<String> mikrostoritve;
+    private List<String> dockerhub;
+    private String opis_projekta;
+    private List<String> clani;
+    private List<String> travis;
+
+    public List<String> getGithub(){
+        return this.github;
+    }
+    public void setGithub(List<String> github) {
+        this.github = github;
+    }
+
+    public List<String> getMikrostoritve(){
+        return this.mikrostoritve;
+    }
+    public void setMikrostoritve(List<String> mikrostoritve) {
+        this.mikrostoritve = mikrostoritve;
+    }
+
+    public List<String> getDockerhub(){
+        return this.dockerhub;
+    }
+    public void setDockerhub(List<String> dockerhub) {
+        this.dockerhub = dockerhub;
+    }
+
+    public String getOpisProjekta(){
+        return this.opis_projekta;
+    }
+    public void setOpisProjekta(String opis) {
+        this.opis_projekta = opis;
+    }
+
+    public List<String> getClani(){
+        return this.clani;
+    }
+    public void setClani(List<String> clani) {
+        this.clani = clani;
+    }
+
+    public List<String> getTravis(){
+        return this.travis;
+    }
+    public void setTravis(List<String> travis) {
+        this.travis = travis;
+    }
+
+    public Info(){
+        this.setGithub(Arrays.asList("https://github.com/SeriesStream/SeriesStream"));
+        this.setMikrostoritve(Arrays.asList());
+        this.setDockerhub(Arrays.asList("https://hub.docker.com/r/lavrinec/series-stream/"));
+        this.setOpisProjekta("SeriesStream je aplikacija, ki je namenjenja spletnim pretočnim vsebinam, med katerimi izstopajo predvsem serije.");
+        this.setClani(Arrays.asList("ml9987"));
+        this.setTravis(Arrays.asList("https://travis-ci.org/SeriesStream/SeriesStream"));
+    }
+
+}
--- src/main/java/com/fri/series/stream/InfoResource.java
+++ src/main/java/com/fri/series/stream/InfoResource.java
@@ -23,38 +23,17 @@ package com.fri.series.stream;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-@Path("customers")
-public class CustomerResource {
+@Path("demo")
+public class InfoResource {
 
     @GET
-    public Response getAllCustomers() {
-        List<Customer> customers = Database.getCustomers();
-        return Response.ok(customers).build();
-    }
-
-    @GET
-    @Path("{customerId}")
-    public Response getCustomer(@PathParam("customerId") String customerId) {
-        Customer customer = Database.getCustomer(customerId);
-        return customer != null
-                ? Response.ok(customer).build()
-                : Response.status(Response.Status.NOT_FOUND).build();
-    }
-
-    @POST
-    public Response addNewCustomer(Customer customer) {
-        Database.addCustomer(customer);
-        return Response.noContent().build();
-    }
-
-    @DELETE
-    @Path("{customerId}")
-    public Response deleteCustomer(@PathParam("customerId") String customerId) {
-        Database.deleteCustomer(customerId);
-        return Response.noContent().build();
+    @Path("info")
+    public Response getInfo() {
+        System.out.println("Info geted");
+        Info info = new Info();
+        return Response.ok(info).build();
     }
 }
