5f2172f73a0fd949080b07089ccd9f8d3c45ce26
Marko <marko.laviner>
2017-12-12 12:39:41 +0100

    health check added

--- src/main/java/com/fri/series/stream/CustomerServiceHealthCheck.java
+++ src/main/java/com/fri/series/stream/CustomerServiceHealthCheck.java
@@ -1,5 +1,24 @@
 package com.fri.series.stream;
 
-public class CustomerServiceHealthCheck {
+import org.eclipse.microprofile.health.Health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Health
+@ApplicationScoped
+public class CustomerServiceHealthCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+
+        if (true) {
+            return HealthCheckResponse.named(CustomerServiceHealthCheck.class.getSimpleName()).up().build();
+        } else {
+            return HealthCheckResponse.named(CustomerServiceHealthCheck.class.getSimpleName()).down().build();
+        }
+
+    }
 
 }
